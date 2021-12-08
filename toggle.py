7d911ca0a85f24b7dce049ce19bb62071cc355e6
Wen Kokke <wenkokke@users.noreply.github.com>
2021-12-08 14:48:56 +0000

    Clear user

--- toggle.py
+++
@@ -1,9 +0,0 @@
-from talon import Module, app, actions, speech_system
-
-mod = Module()
-
-@mod.action_class
-class Actions:
-  def toggle_mute():
-    """Toggle mute/unmute on various videoconferencing apps."""
-    actions.key('shift-ctrl-alt-cmd-m')
