1f17ad4dbf17d0d9110df84a0b7c854550f05820
Wen Kokke <wenkokke@users.noreply.github.com>
2021-12-18 14:02:56 +0000

    cleaned up iterm

--- apps/iterm2/iterm2.py
+++ apps/iterm2/iterm2.py
@@ -10,8 +10,7 @@ and app.bundle: com.googlecode.iterm2
 
 ctx = Context()
 ctx.matches = r"""
-os: mac
-and app.bundle: com.googlecode.iterm2
+app: iterm2
 """
 
 
@@ -19,21 +18,16 @@ and app.bundle: com.googlecode.iterm2
 class Actions:
 
     # support for #terminal
-
-    @staticmethod
     def terminal_clear_screen():
         actions.key("ctrl-l")
 
-    @staticmethod
     def terminal_run_last():
         actions.key("up enter")
 
-    @staticmethod
     def terminal_rerun_search(command: str):
         actions.key("ctrl-r")
         actions.insert(command)
 
-    @staticmethod
     def terminal_kill_current():
         actions.key("ctrl-c")
         actions.key("enter")
