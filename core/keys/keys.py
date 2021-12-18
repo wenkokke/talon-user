5babf6fd0a01c0fd589a95feed4c08f72915dc9b
Wen Kokke <wenkokke@users.noreply.github.com>
2021-12-18 14:02:40 +0000

    changed keys

--- core/keys/keys.py
+++ core/keys/keys.py
@@ -5,66 +5,45 @@ mod = Module()
 ctx = Context()
 mod.tag("keys")
 
-alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(" ")
-default_digits = "zero one two three four five six seven eight nine ten eleven twelve".split(" ")
+alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split()
+digits = "zero one two three four five six seven eight nine ten eleven twelve".split()
 
 mod.list("key_alphabet", desc="The spoken phonetic alphabet")
 ctx.lists["self.key_alphabet"] = {
-    **{alphabet[i]: chr(ord("a") + i) for i in range(len(alphabet))},
-    **{"aalder": "å", "aerlig": "ä", "oesten": "ö"}
+    alphabet[i]: chr(ord("a") + i) for i in range(len(alphabet))
 }
 
 mod.list("key_number", desc="All number keys")
-ctx.lists["self.key_number"] = {default_digits[i]: str(i) for i in range(10)}
+ctx.lists["self.key_number"] = {digits[i]: str(i) for i in range(10)}
 
 mod.list("key_function", desc="All function keys")
-ctx.lists["self.key_function"] = {
-    f"F {default_digits[i]}": f"f{i}" for i in range(1, 13)}
+ctx.lists["self.key_function"] = {f"F {digits[i]}": f"f{i}" for i in range(1, 13)}
 
 mod.list("key_arrow", desc="All arrow keys")
 ctx.lists["self.key_arrow"] = {"up", "down", "left", "right"}
 
-def merge(*args) -> dict:
-    """Merge dictionaries and lists"""
-    result = {}
-    for arg in args:
-        if isinstance(arg, dict):
-            for k, v in arg.items():
-                if v == None:
-                    del result[k]
-                else:
-                    result[k] = v
-        elif isinstance(arg, Iterable):
-            for v in arg:
-                result[v] = v
-        else:
-            raise Exception("Unknown type " + str(type(arg)))
-    return result
-
-
-
 mod.list("key_special", desc="All special keys")
 ctx.lists["self.key_special"] = {
-    "enter"       : "enter",
-    "tab"         : "tab",
-    "delete"      : "delete",
-    "backspace"   : "backspace",
-    "home"        : "home",
-    "end"         : "end",
-    "insert"      : "insert",
-    "escape"      : "escape",
-    "menu"        : "menu",
-    "page up"     : "pageup",
-    "page down"   : "pagedown",
-    "print screen": "printscr"
+    "enter": "enter",
+    "tab": "tab",
+    "delete": "delete",
+    "backspace": "backspace",
+    "home": "home",
+    "end": "end",
+    "insert": "insert",
+    "escape": "escape",
+    "menu": "menu",
+    "page up": "pageup",
+    "page down": "pagedown",
+    "print screen": "printscr",
 }
 
 mod.list("key_modifier", desc="All modifier keys")
 ctx.lists["self.key_modifier"] = {
-    "alt":          "alt",
-    "control":      "ctrl",
-    "shift":        "shift",
-    "super":        "super"
+    "alt": "alt",
+    "control": "ctrl",
+    "shift": "shift",
+    "super": "super",
 }
 
 # Symbols you want available BOTH in dictation and command mode.
@@ -124,9 +103,8 @@ ctx.lists["self.key_symbol"] = {
     "drip": ",",
     "at sign": "@",
     "hash": "#",
-    # "near dash": "–", # en dash
-    # "made dash": "—", # em dash
 }
+
 
 @mod.capture(rule="{self.key_modifier}+")
 def key_modifiers(m) -> str:
@@ -134,8 +112,9 @@ def key_modifiers(m) -> str:
     return "-".join(m.key_modifier_list)
 
 
-@mod.capture(rule="( {self.key_alphabet} | {self.key_number} | {self.key_symbol} "
-              "| {self.key_special} | {self.key_arrow} | {self.key_function} )")
+@mod.capture(
+    rule="( {self.key_alphabet} | {self.key_number} | {self.key_symbol} | {self.key_special} | {self.key_arrow} | {self.key_function} )"
+)
 def key_unmodified(m) -> str:
     "A single key with no modifiers"
     return str(m)
--- core/keys/keys.talon
+++ core/keys/keys.talon
@@ -1,32 +1,12 @@
 # Letters [A-Z]
-{user.key_alphabet}:         key(key_alphabet)
+{user.key_alphabet}: key(key_alphabet)
 
 # Symbol keys: !, %, _
-{user.key_symbol}:           key(key_symbol)
+{user.key_symbol}: key(key_symbol)
 
 # Digits [0-9]
-{user.key_number}:           key(key_number)
+{user.key_number}: key(key_number)
 
-# Special keys.
-(enter | okay):              key(enter)
-tab:                         key(tab)
-escape:                      key(escape)
-caps lock:                   key(capslock)
-num lock:                    key(numlock)
-
-# Special symbols
-new line symbol:             "\\n"
-tab symbol:                  "\\t"
-
-# Modifier(s) + key: "control alpha" or "control win left"
-<user.key_modifiers> <user.key_unmodified>:
+# Press a key with optional modifiers
+press [<user.key_modifiers>] <user.key_unmodified>:
     key("{key_modifiers}-{key_unmodified}")
-
-# All un/non modifier keys: [a-z], [0-9], [F1-F12], arrow, symbols
-key <user.key_unmodified>:   key(key_unmodified)
-
-# Add symbol at end of line and then insert line below
-push {user.key_symbol}:
-    edit.line_end()
-    "{key_symbol}"
-    edit.line_insert_down()
