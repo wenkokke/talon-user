5cd99a8876f795ada222b80cf490e5ac56a94a23
Wen Kokke <wenkokke@users.noreply.github.com>
2021-12-21 13:34:39 +0000

    Factored numbers out into utilities module

--- util/speech.py
+++ util/speech.py
@@ -1,15 +1,15 @@
 from user.util.talon import active_list
+from user.util import numbers
 import re
 
 RE_SNAKE = r"(?<![A-Za-z])([A-Z]?[a-z]+)"
 RE_CAMEL = r"(?<=[A-Za-z])([A-Z][a-z]*)"
 RE_NUM = r"([0-9])"
 RE_WORD = re.compile(f"{RE_SNAKE}|{RE_CAMEL}|{RE_NUM}")
-
+DIGITS = {str(k): v for k, v in zip(range(0,9),numbers.digits)}
 
 def create_spoken_form(text: str) -> str:
     ALPHABET = active_list("user.key_alphabet")
-    DIGITS = active_list("user.key_number")
     chunks = RE_WORD.finditer(text)
     chunks = [m.group(0) for m in chunks]
     chunks = list(map(str.lower, chunks))
@@ -21,6 +21,6 @@ def create_spoken_form(text: str) -> str:
     spoken_form = " ".join(chunks)
     return spoken_form
 
-# assert create_spoken_form("user.get_match1") == "user get match one"
-# assert create_spoken_form("user.macro_play") == "user macro play"
-# assert create_spoken_form("BlockingIOError") == "blocking sit odd error"
+assert create_spoken_form("user.get_match1") == "user get match one"
+assert create_spoken_form("user.macro_play") == "user macro play"
+assert create_spoken_form("BlockingIOError") == "blocking sit odd error"
